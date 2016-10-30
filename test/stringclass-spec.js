14c862f99d569518490bbdbe7c58f49837c7fe18
codemuhammed <codemuhammed@gmail.com>
2016-10-30 22:37:30 +0100

    Implemented tests for hasVowel method

--- test/stringclass-spec.js
+++ test/stringclass-spec.js
@@ -7,3 +7,21 @@ const test = it;
 
 // Imports source code for string class here.
 require('../api/stringclass');
+
+
+//
+suite('hasVowels', () => {
+  test('word that has a vowel in it', () => {
+    assert.equal('hell'.hasVowels(), true);
+  });
+  test('An empty string', () => {
+    assert.equal(''.hasVowels(), false);
+  });
+  test('numbers', () => {
+    assert.equal('12345'.hasVowels(), false);
+  });
+  test('Alpha-numeric string', () => {
+    assert.equal('123abc'.hasVowels(), true);
+  });
+});
+
