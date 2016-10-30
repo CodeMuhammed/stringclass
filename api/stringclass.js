ad7c9ad5d12faf88ba17238e44b928f3fb21424b
codemuhammed <codemuhammed@gmail.com>
2016-10-30 22:38:10 +0100

    Implemented hasVowel method

--- api/stringclass.js
+++ api/stringclass.js
@@ -1,1 +1,13 @@
 // This module extends the String class in Javascript. Using Prototype Oriented Programming, method
+
+/**
+ * String.prototype.hasVowels
+ * This methods takes in a string and tells the client wether it contails vowels ie (a e i o 'or' u)
+ * @return {boolean}.
+ */
+Object.defineProperty(String.prototype, 'hasVowels', {
+  value() {
+    console.log('Has vowel function called');
+    return /[aeiou]/.test(this.toString());
+  },
+});
