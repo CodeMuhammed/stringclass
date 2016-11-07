285e6fa1cb28423abf79410383f95bd5fafc9d79
codemuhammed <codemuhammed@gmail.com>
2016-11-07 01:19:54 +0100

    Implemented tests for numberWords function

--- test/stringclass-spec.js
+++ test/stringclass-spec.js
@@ -216,3 +216,17 @@ suite('getMiddle', () => {
     assert.isTrue('reads'.getMiddle() === 'a');
   });
 });
+
+
+//
+suite('numberWords', () => {
+  test('An empty string', () => {
+    assert.isTrue(testData.empty.getMiddle() === 'Invalid number string');
+  });
+  test('A space character', () => {
+    assert.isTrue(testData.space.getMiddle() === 'Invalid number string');
+  });
+  test('number', () => {
+    assert.isTrue('325'.numberWords() === 'three two five');
+  });
+});
